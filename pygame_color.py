51266255cd350e7ea17cb48b16e77d0c7d112cc3
guofei.sun <1316390245@qq.com>
2019-11-14 19:02:28 +0800

    2019-11-14 19:06

---
+++ pygame_color.py
@@ -0,0 +1,63 @@
+import sys
+import pygame
+from pygame import Surface
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Pygame图形绘制")
+
+YELLOW = 255, 255, 0
+ORANGE = 255, 165, 0
+BLUE = 0, 0, 255
+GREEN = 0, 255, 0
+RED = 255, 0, 0
+WHITE = 255, 255, 255
+
+LINEN = 250, 240, 230
+
+size = (600, 400)
+surface = Surface(size)
+surface.set_colorkey((0, 0, 0))
+# surface.set_alpha(200)
+
+color_dict = {'u': YELLOW, 'd': WHITE, 'r': BLUE, 'l': GREEN, 'f': ORANGE, 'b': RED}
+pos = (1, 1, 1)
+color = ('u', 'b', 'r')
+
+
+class DrawRubikS:
+    def __init__(self, pos_color_tuple):
+        for pos_color in pos_color_tuple:
+            self.draw(pos_color)
+
+    def draw(self, pos_color):
+        pos, color = pos_color
+        pos = (1, 1, 1)
+        color = ('u', 'b', 'r')
+        pass
+
+
+# 绘制矩形
+# r1rect = pygame.draw.rect(screen, GOLD, (100,100,200,100), 5)
+# r2rect = pygame.draw.rect(screen, RED, (210,210,200,100), 0)
+
+# p4poly = pygame.draw.polygon(surface, GREEN, [(100, 200), (150, 150), (150, 250), (100, 300)], 0)
+# p5poly = pygame.draw.polygon(surface, WHITE, [(100, 300), (200, 300), (250, 250), (150, 250)], 0)
+# p6poly = pygame.draw.polygon(surface, RED, [(150, 150), (150, 250), (250, 250), (250, 150)], 0)
+
+# p4poly = pygame.draw.polygon(surface, LINEN, [(600, 200), (490, 190), (490, 340), (600, 350)], 0)  #
+# p5poly = pygame.draw.polygon(surface, LINEN, [(600, 200), (200, 300), (250, 250), (150, 250)], 0)
+# p6poly = pygame.draw.polygon(surface, LINEN, [(150, 150), (150, 250), (250, 250), (250, 150)], 0)
+p0poly = pygame.draw.polygon(surface, YELLOW, [(600, 200), (450, 150), (450, 350), (600, 400)], 0)
+p1poly = pygame.draw.polygon(surface, ORANGE, [(600, 200), (750, 150), (750, 350), (600, 400)], 0)
+p2poly = pygame.draw.polygon(surface, BLUE, [(600, 200), (450, 150), (600, 100), (750, 150)], 0)
+screen.blit(surface, (0, 0))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    pygame.display.update()
--- pygame_study.py
+++ pygame_study.py
@@ -1,56 +1,54 @@
 from math import pi
 
-import pygame,sys
+import sys
+import pygame
+from pygame import Surface
 
 pygame.init()
 
 screen = pygame.display.set_mode((600, 400))
-screen1 = pygame.display.set_mode((600, 400))
-# screen = pygame.Surface((600, 400), pygame.SRCALPHA, 32)
-
-# windowSurface = pygame.display.set_mode((500, 600), pygame.DOUBLEBUF)
-# pygame.draw.rect(windowSurface, pygame.Color(255, 255, 255, 128), pygame.Rect(0, 200, 500, 250))
-#
-# s = pygame.Surface((500,500))  # the size of your rect
-# s.set_alpha(128)                # alpha level
-# s.fill((255,255,255))           # this fills the entire surface
-# windowSurface.blit(s, (0, 0))    # (0,0) are the top-left coordinates
+# screen.fill((255, 255, 255))
 
 pygame.display.set_caption("Pygame图形绘制")
-# srf = screen.convert_alpha()
-# srf.fill((255, 255, 255, 0))
-# screen = srf
 
-
-
-# GOLD = 255, 251, 0
-# RED = pygame.Color('red')
-# WHITE = 255, 255, 255
-# GREEN = pygame.Color('green')
-YELLOW = 255, 255, 0, 52
-ORANGE = 255, 165, 0, 1
-BLUE = 0, 0, 255, 1
+YELLOW = 255, 255, 0
+ORANGE = 255, 165, 0
+BLUE = 0, 0, 255
 GREEN = 0, 255, 0
 RED = 255, 0, 0
 WHITE = 255, 255, 255
-YELLOW = pygame.Color(*YELLOW)
-print(YELLOW.a)
+
+LINEN = 250, 240, 230
+
+# size = (150, 150)
+size = (600, 400)
+surface = Surface(size)
+surface.set_colorkey((0, 0, 0))
+# surface.set_alpha(200)
 
 
 # 绘制矩形
 # r1rect = pygame.draw.rect(screen, GOLD, (100,100,200,100), 5)
 # r2rect = pygame.draw.rect(screen, RED, (210,210,200,100), 0)
 
-p4poly = pygame.draw.polygon(screen, GREEN, [(100, 200), (150, 150), (150, 250), (100, 300)], 0)
-p5poly = pygame.draw.polygon(screen, WHITE, [(100, 300), (200, 300), (250, 250), (150, 250)], 0)
-p6poly = pygame.draw.polygon(screen, RED, [(150, 150), (150, 250), (250, 250), (250, 150)], 0)
+# p4poly = pygame.draw.polygon(surface, GREEN, [(100, 200), (150, 150), (150, 250), (100, 300)], 0)
+# p5poly = pygame.draw.polygon(surface, WHITE, [(100, 300), (200, 300), (250, 250), (150, 250)], 0)
+# p6poly = pygame.draw.polygon(surface, RED, [(150, 150), (150, 250), (250, 250), (250, 150)], 0)
 # import time
 # time.sleep(3)
-p0poly = pygame.draw.polygon(screen1, YELLOW, [(100, 200), (150, 150), (250, 150), (200, 200)], 0)
-# p1poly = pygame.draw.polygon(screen, ORANGE, [(100, 200), (100, 300), (200, 300), (200, 200)], 0)
-# p2poly = pygame.draw.polygon(screen, BLUE, [(200, 200), (200, 300), (250, 250), (250, 150)], 0)
+p4poly = pygame.draw.polygon(surface, LINEN, [(100, 200), (150, 150), (150, 250), (100, 300)], 0)
+p5poly = pygame.draw.polygon(surface, LINEN, [(100, 300), (200, 300), (250, 250), (150, 250)], 0)
+p6poly = pygame.draw.polygon(surface, LINEN, [(150, 150), (150, 250), (250, 250), (250, 150)], 0)
+p0poly = pygame.draw.polygon(surface, YELLOW, [(100, 200), (150, 150), (250, 150), (200, 200)], 0)
+p1poly = pygame.draw.polygon(surface, ORANGE, [(100, 200), (100, 300), (200, 300), (200, 200)], 0)
+p2poly = pygame.draw.polygon(surface, BLUE, [(200, 200), (200, 300), (250, 250), (250, 150)], 0)
+screen.blit(surface, (0, 0))
+# screen.blit(white_sfc, (0, 0))
+# screen.blit(red_sfc, (0, 0))
+# screen.blit(yellow_sfc, (0, 0))
+# screen.blit(orange_sfc, (0, 0))
+# screen.blit(blue_sfc, (0, 0))
 
-#
 
 # 绘制笑脸
 # e1rect = pygame.draw.ellipse(screen, GREEN, (50,50,500,300), 3)
